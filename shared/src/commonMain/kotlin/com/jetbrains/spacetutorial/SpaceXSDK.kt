47c128f8946a5b3a939441686b44a9bd3418cd39
Rafael Antunes <rmrantunes.dev@gmail.com>
2026-04-15 21:06:38 -0400

    feat: add SpaceXSDK and cache layer

---
+++ shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/SpaceXSDK.kt
@@ -0,0 +1,21 @@
+package com.jetbrains.spacetutorial
+
+import com.jetbrains.spacetutorial.cache.Database
+import com.jetbrains.spacetutorial.cache.DatabaseDriverFactory
+import com.jetbrains.spacetutorial.network.SpaceXApi
+
+class SpaceXSDK(databaseDriverFactory: DatabaseDriverFactory, val api: SpaceXApi) {
+    private val database = Database(databaseDriverFactory)
+
+    @Throws(Exception::class)
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedValues = database.getAllLaunches()
+        return if(cachedValues.isNotEmpty() && !forceReload) {
+            cachedValues
+        } else {
+            api.getAllLaunches().also {
+                database.clearAndCreateLaunches(it)
+            }
+        }
+    }
+}
